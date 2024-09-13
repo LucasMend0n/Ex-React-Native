acc24ae98f2bf9e88040b195b37afce7f9261857
Felipe Felix <115411900+FelipeFelixhub@users.noreply.github.com>
2024-09-13 22:28:20 +0000

    Tela cadastro layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{title:"Bar do GG"}} />
+      <Stack.Screen name="Cadastro"/>
     </Stack>
   );
 }
